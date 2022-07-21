613f7bb851f1fcc6d7afa1079fd11a5d575dd710
Artur Zdolinski <artur@zdolinski.com>
2022-07-21 12:12:06 +0200

    modified:   jcollect-sh/junos_SRX5400.sh

--- jcollect-sh/junos_SRX5400.sh
+++ jcollect-sh/junos_SRX5400.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# bash# sh junos_SRX5400.sh > /var/tmp/`date +"%Y%m%d_%H%M%S"`-"$HOST"-cprod.txt
 hostname=$(hostname -s)
 exe() {
 echo ">======================================================================"
@@ -11,8 +12,21 @@ echo ""; }
 
 
 n=1
-while [ $n -lt 10 ]
+while [ $n -le 10 ]
 do
+  
+  while true; do
+    # Run script exactly in 30sec intervals - at: hh:mm:00 -or- hh:mm:30
+    d=$(date +%S)
+    if [ $d -eq 0 -o $d -eq 30 ]; then
+      echo -n "x== n=$n "; date '+%Y-%m-%d %H:%M:%S %Z [%z] | %s'
+      break 1
+    else
+      #echo "x== waiting - sec=$d"
+      sleep 0.2
+    fi
+  done
+  
   exe srx-cprod.sh -s spu -c "show xlr cpu detail all"
   exe srx-cprod.sh -s spu -c "show dpq drop"
   exe srx-cprod.sh -s spu -c "show dpq stats"
@@ -24,5 +38,4 @@ do
   exe srx-cprod.sh -s spu -c "show usp flow counters resource-mgr-cpu"
   exe srx-cprod.sh -s spu -c "show usp resource-manager cpu"
   n=$(( n+1 ))
-  sleep 30
 done
